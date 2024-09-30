1afebce5a368e074836ab5ae8403a35303af07da
NicoG1102 <colacho2776@hotmail.com>
2024-09-30 12:39:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv('../vehicles_us.csv')
+df = pd.read_csv('./vehicles_us.csv')
 
 st.header("Car Advertisement Dashboard")
 
