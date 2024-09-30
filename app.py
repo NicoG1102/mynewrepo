4b26728e2cac72a435f33d07bd23e554b29453c7
NicoG1102 <colacho2776@hotmail.com>
2024-09-29 21:14:27 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('../vehicles_us.csv')
+
+st.header("Car Advertisement Dashboard")
+
+fig_hist = px.histogram(df, x='type')
+st.plotly_chart(fig_hist)
+
+fig_scatter = px.scatter(df, x='model_year', y='condition')  # Adjust column names
+st.plotly_chart(fig_scatter)
+
+show_histogram = st.checkbox("Show Histogram")
+if show_histogram:
+    st.plotly_chart(fig_hist)
+
+
